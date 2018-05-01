253c498a7b854a20707b6e45d99d5cfb2760d4b3
badgeri <alexander.ostman88@gmail.com>
2018-05-01 19:48:15 +0200

    added enemy 1 who always target closest player ball and moves to intercept it.

---
+++ New Unity Project/Assets/EnemyBall/EnemyMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    private float mMaxVelocity = 10.0f;
+    private float mMinVelocity = -10.0f;
+    private float mCurrentVelocity = 0;
+
+    // Update is called once per frame
+    void Update ()
+    {
+        findDirection();
+    }
+
+    private void findDirection()
+    {
+        float closestBallDistance = float.MaxValue;
+        var playerBalls = GameObject.FindGameObjectsWithTag("Player");
+        GameObject closestBall = playerBalls[0];
+        foreach (GameObject playerBall in playerBalls)
+        {
+            float dist = Vector3.Distance(playerBall.transform.position, transform.position);
+            if (dist < closestBallDistance)
+            {
+                closestBallDistance = dist;
+                closestBall = playerBall;
+            }
+        }
+        var heading = closestBall.transform.position - transform.position;
+        var distance = heading.magnitude;
+        var direction = heading / distance;
+
+        Rigidbody rb = GetComponent<Rigidbody>();
+        mCurrentVelocity = Mathf.Min(rb.velocity.magnitude + 1, mMaxVelocity);
+        var angle = Mathf.Atan2(direction.z, direction.x);
+        var Vz = Mathf.Sin(angle) * mCurrentVelocity;
+        var Vx = Mathf.Cos(angle) * mCurrentVelocity;
+        rb.velocity = new Vector3(Vx, rb.velocity.y, Vz);
+    }
+}
