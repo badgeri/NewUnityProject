afc3e063e38138ae533ea550babf5a1bfab8fea0
badgeri <alexander.ostman88@gmail.com>
2018-05-02 12:06:26 +0200

    Fixed tests for enemy ball

--- New Unity Project/Assets/Tests/EnemyBallTests.cs
+++ New Unity Project/Assets/Tests/EnemyBallTests.cs
@@ -3,15 +3,30 @@ using UnityEngine.TestTools;
 using NUnit.Framework;
 using System.Collections;
 
-public class EnemyBallTests {
-	// A UnityTest behaves like a coroutine in PlayMode
-	// and allows you to yield null to skip a frame in EditMode
-	[UnityTest]
-	public IEnumerator EnemyBallTestsWithEnumeratorPasses() {
-		// Use the Assert class to test conditions.
-		// yield to skip a frame
-		yield return null;
 
+/// <summary>
+/// Test class for Enemy Ball
+/// </summary>
+public class EnemyBallTests
+{
+
+    /// <summary>
+    /// Verify that Enemy Ball moves to collide closest player ball.
+    /// </summary>
+    /// <returns></returns>
+    [UnityTest]
+    public IEnumerator _EnemyBallCollideClosestPlayerBall()
+    {
         Assert.True(Application.CanStreamedLevelBeLoaded("MultipleBallAndPlaneScene"), "Level could not be loaded.");
+        AsyncOperation loadSceneAsync = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("MultipleBallAndPlaneScene");
+        yield return loadSceneAsync;
+        Debug.Log("Loading complete");
+        yield return new WaitForSeconds(2.5f);
+        var enemyBall = GameObject.FindWithTag("Enemy");
+        Assert.True(enemyBall.GetComponent<OnCollision>().getTouchingPlayerBall(), "Enemy ball is expected to touch player ball.");
+        var playerBalls = GameObject.FindGameObjectsWithTag("Player");
+        Assert.True(playerBalls.Length == 2);
+        Assert.True(playerBalls[0].GetComponent<OnCollision>().getTouchingEnemyBall(), "First player ball is expected to touch enemy ball.");
+        Assert.False(playerBalls[1].GetComponent<OnCollision>().getTouchingEnemyBall(), "Second player ball is not expected to touch enemy ball.");
     }
 }
