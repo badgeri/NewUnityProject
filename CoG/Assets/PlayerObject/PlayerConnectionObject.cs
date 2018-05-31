f221f03adbc4c6b7b005e0c6d7a26f3ca17ea959
badgeri <alexander.ostman88@gmail.com>
2018-05-31 22:34:09 +0200

    Setup multiplayer

---
+++ CoG/Assets/PlayerObject/PlayerConnectionObject.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerConnectionObject : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        //Is this actually my own local PlayerConnectionObject?
+        if ( isLocalPlayer == false ) {
+            //This object belongs to another player.
+            return;
+        }
+
+        // Since the PlayerConnectionObject is invisible and not part of the world, create something physical to move around!
+        Debug.Log("PlayerConnectionObject::Start -- Spawning my own personal unit.");
+
+        // Command the server to spawn my unit
+        CmdSpawnMyUnit();
+	}
+
+    public GameObject PlayerUnitPrefab;
+	
+	// Update is called once per frame
+	void Update () {
+		// Remember: Update runs on everyones computer, whether or not they own this particular player object.
+        if (isLocalPlayer == false) {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S)) {
+            CmdSpawnMyUnit();
+        }
+	}
+
+
+    ///COMMANDS
+    ///Special functions that ONLY get executed on the server.
+    ///
+
+    [Command]
+    void CmdSpawnMyUnit() {
+        // We are guaranteed to be on the server right now.
+        GameObject myPlayerUnit = Instantiate(PlayerUnitPrefab);
+
+        // Now that the object exists on the server, propagate it to all the clients and also wire up the NetworkIdentity.
+        NetworkServer.SpawnWithClientAuthority(myPlayerUnit, connectionToClient);
+    }
+}
---
+++ CoG/Assets/PlayerObject/PlayerUnit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+// A PlayerUnit is a unit controlled by a player (will be renamed PlayerHero later!)
+
+public class PlayerUnit : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Verify that user is allowed to control object
+        if (hasAuthority == false) {
+            return;
+        }
+
+		if (Input.GetKeyDown(KeyCode.Space)) {
+            this.transform.Translate(0, 1, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D)) {
+            Destroy(gameObject);
+        }
+	}
+}
