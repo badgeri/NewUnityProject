08fca8e25dd9b744d5f0ac0048f4d20f920fc277
Jonas <s4nk13@gmail.com>
2018-06-30 16:53:58 +0200

    Created Makefile to build and run tests, also created buildscripts

---
+++ CoG/Assets/Tests/PickTests.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class PickTests {
+
+    [Test]
+    public void SuccessTest() {
+        // Use the Assert class to test conditions.
+        Assert.AreEqual(1, 1, 0, "One and one are equal");
+
+    }
+    [Test]
+    public void FailTest() {
+        // Use the Assert class to test conditions.
+        Assert.AreEqual(1, 0, 0, "One and zero is not equal");
+
+    }
+
+    // A UnityTest behaves like a coroutine in PlayMode
+    // and allows you to yield null to skip a frame in EditMode
+    [UnityTest]
+    public IEnumerator NewTestScriptWithEnumeratorPasses() {
+        // Use the Assert class to test conditions.
+        // yield to skip a frame
+        Assert.AreEqual(1, 1, 0);
+        yield return null;
+    }
+}
