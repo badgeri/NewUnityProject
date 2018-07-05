3d4c0937bf5dd6d3e52f7f82e3fd7c9cdbb30a4c
sandqvist <39836769+sandqvist@users.noreply.github.com>
2018-07-05 12:52:26 +0200

    Moved find player to lib/HandlerPlayer (#7)

---
+++ CoG/Assets/lib/HandlerPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandlerPlayer {
+    public static bool ActiveGameObject(Collider collider, out GameObject outGameObject)
+    {
+        outGameObject = null;
+        if (collider.gameObject.tag == "Playerobject")
+        {
+            GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("Player");
+            foreach (GameObject gObject in gameObjects)
+            {
+                if (collider.GetComponentInParent<PlayerUnit>().parentNetworkId == gObject.GetComponent<PlayerConnectionObjectScript>().netId && gObject.GetComponent<PlayerConnectionObjectScript>().hasAuthority)
+                {
+                    outGameObject = gObject;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
