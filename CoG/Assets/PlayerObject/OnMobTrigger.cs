a6b288ce8214aa01378fc73f472026af8fdee9f5
Jonas <s4nk13@gmail.com>
2018-07-05 06:53:40 +0200

    Created a mob, when enter it launches a menu to select attack or retreat

---
+++ CoG/Assets/PlayerObject/OnMobTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+
+public class OnMobTrigger : NetworkBehaviour
+{
+    public GameObject canvas;
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Playerobject")
+        {
+            CmdDestroyGameObject(this.gameObject);
+            GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("Player");
+            foreach (GameObject gObject in gameObjects)
+            {
+                if (collider.GetComponentInParent<PlayerUnit>().parentNetworkId == gObject.GetComponent<PlayerConnectionObjectScript>().netId && gObject.GetComponent<PlayerConnectionObjectScript>().hasAuthority)
+                {
+                    canvas.SetActive(true);
+                }
+            }
+        }
+    }
+
+    [Command]
+    private void CmdDestroyGameObject(GameObject gObject)
+    {
+        Destroy(gObject);
+    }
+
+
+}
